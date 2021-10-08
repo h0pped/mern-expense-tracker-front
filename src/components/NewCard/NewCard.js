db2974aa240bae9f1b81d1f1d2058a99bc84b660
h0pped <notawril@gmail.com>
2021-10-08 10:31:32 +0200

    feat: add new card form

--- src/components/NewCard/NewCard.js
+++ src/components/NewCard/NewCard.js
@@ -1,9 +1,56 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./styles.scss";
 export default React.forwardRef(({}, ref) => {
+  const [inputs, setInputs] = useState({});
+
+  const inputHandler = (e) => {
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+    setInputs({
+      ...inputs,
+      [inputName]: inputValue,
+    });
+  };
+  const submitCardHandler = () => {
+    console.log(inputs);
+  };
   return (
     <div ref={ref} className="card-container">
-      <p>NewCard</p>
+      <div className="new-card">
+        <h3>Add new Card</h3>
+
+        <form>
+          <div className="form-input">
+            <label htmlFor="title">Title</label>
+            <div className="input-container">
+              <input
+                onChange={inputHandler}
+                type="text"
+                name="title"
+                id="titleInput"
+                placeholder="Savings"
+              />
+            </div>
+          </div>
+          <div className="form-input">
+            <label htmlFor="balance">Initial balance</label>
+            <div className="input-container">
+              <input
+                onChange={inputHandler}
+                type="number"
+                step="0.01"
+                name="balance"
+                id="balanceInput"
+                placeholder="2.99"
+              />
+              <label htmlFor="balance">$</label>
+            </div>
+          </div>
+        </form>
+      </div>
+      <button type="submit" onClick={submitCardHandler}>
+        Add
+      </button>
     </div>
   );
 });
